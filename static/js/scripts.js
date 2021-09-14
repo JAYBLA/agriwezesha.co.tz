89ae4d0c5e5808754eb47f7758529e3ad384b27c
JAYBLA <jayblaenterprises@gmail.com>
2021-09-14 12:34:50 +0300

    lodaaa

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,7 +5,7 @@
     //Preloader
     Royal_Preloader.config({
         mode           : 'logo',
-        logo           : '../static/images/logo.svg',
+        logo           : 'https://agriwezesha.co.tz/static/images/logo.svg',
         logo_size      : [187, 187],
         showProgress   : true,
         showPercentage : true,
